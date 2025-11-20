77a1b205eb88feffc7787612c4549151242b5f44
sergivv <sergivv@gmail.com>
2025-11-20 18:29:01 +0100

    Oled configuration

--- Control/src/main.cpp
+++ Control/src/main.cpp
@@ -1,18 +1,50 @@
-#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Configuración OLED
+Adafruit_SSD1306 display(128, 64, &Wire);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+bool setupOLED()
+{
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("Error OLED");
+    return false;
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.display();
+  return true;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void displayMessage(const char* line1, const char* line2, 
+  const char* line3, const char* line4, 
+  const char* line5, const char* line6,
+  const char* line7, const char* line8)
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(line1);
+  display.println(line2);
+  display.println(line3);
+  display.println(line4);
+  display.println(line5);
+  display.println(line6);
+  display.println(line7);
+  display.println(line8);
+  display.display();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+  Serial.begin(115200);
+
+  if (!setupOLED())
+    ESP.restart();
+}
+
+void loop()
+{
+    displayMessage("ACSControl", "Inicializando...", "1", "2", "3", "4", "5", "Listo!");
+    delay(1000);
 }
